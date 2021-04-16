77a27406050fb277eabcff2dc6e3469ec951beb4
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2021-04-16 10:22:53 +0530

    Create VCOUPLE.cpp

---
+++ VCOUPLE.cpp
@@ -0,0 +1,68 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+#define yes cout<<"yes\n";
+#define no cout<<"no\n";
+#define YES cout<<"YES\n";
+#define NO cout<<"NO\n";
+#define ll long long int
+#define pb push_back
+#define mod 1000000007
+#define nl cout<<"\n";
+#define loop1(m) for(int i=0;i<m;i++) 
+#define loop2(n,m) for(auto i=n;i<=m;i++)
+#define count1s(n) __builtin_popcount(n)
+bool psqrt(ll x) 
+{   
+  long double sr = sqrt(x); 
+  return ((sr - floor(sr)) == 0); 
+}
+int lcm(int a, int b)
+{
+    return a / __gcd(a, b) * b;
+}
+bool sortbysec(const pair<int,int> &a, 
+              const pair<int,int> &b) 
+{ 
+    return (a.second < b.second); 
+}
+bool isprime(ll n) 
+{ 
+    if (n <= 1)  return false; 
+    if (n <= 3)  return true; 
+    if (n%2 == 0 || n%3 == 0) return false; 
+    for (int i=5; i*i<=n; i=i+6) 
+        if (n%i == 0 || n%(i+2) == 0) 
+           return false; 
+    return true; 
+}
+int countdigits(ll n)
+{
+	return floor(log10(n)+1);
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+    
+    
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        ll a[n],b[n];
+        loop1(n) cin>>a[i];
+        loop1(n) cin>>b[i];
+        sort(a,a+n,greater<int>());
+        sort(b,b+n);
+        ll ans=0;
+        for(int i=0;i<n;i++)
+        {
+            ans=max(ans,a[i]+b[i]);
+        }
+        cout<<ans<<endl;
+    }
+}
