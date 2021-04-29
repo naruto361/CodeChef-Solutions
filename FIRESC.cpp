5f665d1933219cf24bf8502902e65068c06b4ac1
Saksham Rohilla <54116115+naruto361@users.noreply.github.com>
2021-04-29 17:38:17 +0530

    Update FIRESC.cpp

--- FIRESC.cpp
+++ FIRESC.cpp
@@ -1,27 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std;
-void dfs(int x,vector<int> &vis,vector<vector<int>> v,int &u)
+#define ll long long 
+#define mod 1000000007
+vector<vector<int>> v(100001);
+vector<bool> vis(100001);
+void dfs(int x,int &k)
 {
-	vis[x]=1;
-    u++;
-	for(int i=0;i<v[x].size();i++)
+	vis[x]=1;k++;
+	for(int child:v[x])
 	{
-		int k=v[x][i];
-		if(vis[k]==0) dfs(k,vis,v,u);
+		if(vis[child]==0) dfs(child,k);
 	}
 }
 int main()
 {
-    int mod=1e9+7;
+    ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
     int t;
     cin>>t;
     while(t--)
     {
+        v.clear();vis.clear();
         int n,m;
         // vertex and edges
         cin>>n>>m;
-        vector<vector<int>> v(n+1);
-        vector<int> vis(n+1,0);
+        v.resize(n+1);
+        vis.resize(n+1);
         for(int i=0;i<m;i++)
         {
             int a,b;
@@ -29,18 +33,20 @@ int main()
             v[a].push_back(b);
             v[b].push_back(a);
         }
-        int ans=1;
+        ll ans=1;
         int cnt=0;
         for(int i=1;i<=n;i++)
         {
             if(vis[i]==0)
             {
-                int u=0;
-                cnt++;dfs(i,vis,v,u);
-                ans=(ans%mod * u%mod)%mod;
+                cnt++;
+                int k=0;
+                dfs(i,k);
+                ans=(ans%mod * k%mod)%mod;
             }
         }
         cout<<cnt<<" "<<ans<<endl;
+        //v.clear();
     }
 	return 0;
 }
